c00799bb2972caf482f8908af196a09fb8d5cd1a
Notdetected <whoa75039@gmail.com>
2020-03-23 11:52:23 +0700

    deploy lagi a

--- app.py
+++ app.py
@@ -31,25 +31,35 @@ def start():
                                 tw.post_tweet_with_media(message, dms[i]['media'],dms[i]['shorted_media_url'], dms[i]['type'])
                                 tw.delete_dm(id)
                         else:
-                            if ".quotem" in message:
-                                if "https://" not in message and "http://" not in message:
-                                    message = message.replace(".quotem", "[Kudus Quote]")
-                                    screen_name = tw.get_user_screen_name(sender_id)
-                                    media.download_image()
-                                    media.process_image(message, screen_name)
-                                    tw.post_tweet()
-                                    tw.delete_dm(id)
-                                else:
-                                    media.download_image()
-                                    media.process_image(message, None)
-                                    tw.post_tweet()
-                                    tw.delete_dm(id)
-                            else:
-                                print("DM deleted because its empty..")
-                                tw.delete_dm(id)
-                    else:
-                        print("DM will be deleted because does not contains keyword..")
-                        tw.delete_dm(id)
+							if "https://" not in message and "http://" not in message:
+								if ".quotem" in message:
+									if len(message) is not 0:
+										if dms[i]['media'] is None:
+											print("DM will be posted")
+											message = message.replace(".quotem", "[Kudus Quote]")
+											screen_name = tw.get_user_screen_name(sender_id)
+											media.download_image()
+											media.process_image(message, screen_name)
+											tw.post_quote()
+											tw.delete_dm(id)
+										else:
+											media.download_image()
+											media.process_image(message, None)
+											tw.post_tweet()
+											tw.delete_dm(id)
+									else:
+										print("DM deleted because its empty..")
+										tw.delete_dm(id)
+								else:
+									print("DM will be deleted because does not contains keyword..")
+									tw.delete_dm(id)
+							else:
+								print("DM deleted because its empty..")
+								tw.delete_dm(id)
+					else:
+						print("DM will be deleted because does not contains keyword..")
+						tw.delete_dm(id)
+
 
             dms = list()
 
