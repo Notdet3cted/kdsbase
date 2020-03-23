35223fb125332761b0fadebb614ba27ae0ef187b
Notdetected <whoa75039@gmail.com>
2020-03-23 17:53:31 +0700

    depl

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def start():
             print("Direct message is empty...")
             dms = tw.read_dm()
             if len(dms) is 0:
-                time.sleep(60)
+                time.sleep(600)
 
 if __name__ == "__main__":
     start()
